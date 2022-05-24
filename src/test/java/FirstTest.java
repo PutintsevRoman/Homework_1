7a65463de526c9df836004d581039056295dc2ca
Putintsev_Roman <putintsevromann@yandex.ru>
2022-05-24 16:38:15 +0300

    Refactor 3 tests

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Test;
 public class FirstTest {
     @Test
     void firstTest(){
-        Assertions.assertTrue(3>2);
+        Assertions.assertTrue(5>2);
     }
     void firstTest_1(){
-        Assertions.assertTrue(4>2);
+        Assertions.assertTrue(4+8>2);
     }
     void firstTest_2(){
-        Assertions.assertTrue(2>=2);
+        Assertions.assertTrue(2+7-3>=2);
     }
 }
